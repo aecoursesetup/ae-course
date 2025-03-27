d963f3f9a9aa82b0df2edfed03880cb69f37e6f1
ae.course.setup@gmail.com <ae.course.setup@gmail.com>
2025-03-27 21:47:33 +0000

    Date time conversions

--- models/staging/stg_transactions.sql
+++ models/staging/stg_transactions.sql
@@ -4,10 +4,13 @@ select
     order_id,
     visit_id,
     user_id,
-    order_created_at,
+    order_created_at as order_created_utc,
+    datetime(timestamp(order_created_at), "America/New_York") as order_created_et,
     location_id,
     shipping_carrier,
     shipping_method,
-    estimated_delivery_date,
-    delivered_at
+    estimated_delivery_date as estimated_deilvery_utc,
+    datetime(timestamp(estimated_delivery_date), "America/New_York") as estimated_delivery_et,
+    delivered_at as delivered_at_utc,
+    datetime(timestamp(delivered_at), "America/New_York") as delivered_at_et
 from {{ source('class_source_data','transactions') }}
