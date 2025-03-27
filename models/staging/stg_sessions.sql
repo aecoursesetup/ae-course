92477d6dd4fccc36e3c2fe4d26c5b567f427c6c6
ae.course.setup@gmail.com <ae.course.setup@gmail.com>
2025-03-27 20:23:49 +0000

    added columns

--- models/staging/stg_sessions.sql
+++ models/staging/stg_sessions.sql
@@ -1,5 +1,14 @@
 {{ config(materialized='view', tags=['staging']) }}
 
 select
-*
+    visit_id,
+    visit_start_time,
+    visit_end_time,
+    device_type,
+    browser,
+    pageview_count,
+    spend_type,
+    attributed_channel,
+    attributed_subchannel,
+    session_metadata
 from {{ source('class_source_data','sessions') }}
--- models/staging/stg_transactions.sql
+++ models/staging/stg_transactions.sql
@@ -1,5 +1,13 @@
 {{ config(materialized='view', tags=['staging']) }}
 
 select
-*
+    order_id,
+    visit_id,
+    user_id,
+    order_created_at,
+    location_id,
+    shipping_carrier,
+    shipping_method,
+    estimated_delivery_date,
+    delivered_at
 from {{ source('class_source_data','transactions') }}
